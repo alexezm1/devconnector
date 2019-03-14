c74f20f3a3a9baa4ea07d7f259104e2eb465d616
alexezm1 <alex_speed_96@hotmail.com>
2019-03-13 20:27:57 -0700

    Created validations, routes and jwt auth

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://alexzam1:alexzam1@ds111771.mlab.com:11771/devconnectorcourse"
+    "mongodb://alexzam1:alexzam1@ds111771.mlab.com:11771/devconnectorcourse",
+  secretOrKey: "secret"
 };
