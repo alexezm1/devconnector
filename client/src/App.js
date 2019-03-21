26fc5a80d46a0a8f521a6c462285e4fac696a707
alexezm1 <alex_speed_96@hotmail.com>
2019-03-20 20:25:21 -0700

    added login and register routes, worked on register states

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import './App.css';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router><div className="App">
         <Navbar/>
-        <Landing/>
+        <Route exact path = "/" component = {Landing}/>
+        <div className="container">
+          <Route exact path = "/register" component = {Register}/>
+          <Route exact path = "/login" component = {Login}/>
+        </div>
         <Footer/>
       </div>
+      </Router>
     );
   }
 }
