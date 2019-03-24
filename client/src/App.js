b61afa217a34d0659c2e93cee997c3b70a05f641
alexezm1 <alex_speed_96@hotmail.com>
2019-03-23 19:52:16 -0700

    Finished Dashboard, add experience and education. Almost Done!

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,9 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 
 import './App.css';
+import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -60,6 +63,27 @@ class App extends Component {
                   exact
                   path="/create-profile"
                   component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
                 />
               </Switch>
             </div>
