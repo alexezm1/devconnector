f52d0f893095b64552c3adb4ac7ea5116b4305a5
alexezm1 <alex_speed_96@hotmail.com>
2019-03-29 13:12:43 -0700

    almost donde with profile display section

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import AddEducation from './components/add-credentials/AddEducation';
 
 import './App.css';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -57,6 +58,7 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles}/>
+              <Route exact path="/profile/:handle" component={Profile}/>
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
